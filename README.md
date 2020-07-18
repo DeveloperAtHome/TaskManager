fefb6b579ea44f7e8b5114272351a7786f3cc5c3
Nikola-Radojicic-94 <radojicic.programer@gmail.com>
2020-07-18 22:41:40 +0200

    refactored code

---
+++ README.md
@@ -0,0 +1,6 @@
+# TaskManager
+Platform for managing project tasks
+
+Execute 'npm install' inside root folder.
+Execute 'npm install' inside /webServer folder.
+To start application run 'npm run start' inside root folder.
--- pre-build.js
+++ pre-build.js
@@ -13,19 +13,3 @@ stream.once('open', () => {
   stream.write('export default componentTemplateRegister');
   stream.end()
 })
-
-// const components = fs.readdirSync('./src/components');
-// components.forEach(component => {
-//   componentTemplate = fs.readFileSync(`./src/components/${component}/${component}.html`).toString();
-//   templates.push({
-//     name: component,
-//     template: componentTemplate
-//   });
-// });
-
-// var stream = fs.createWriteStream("./src/tmp.js");
-// stream.once('open', () => {
-//   stream.write(`const templates = ${JSON.stringify(templates)}\n`);
-//   stream.write('export default templates');
-//   stream.end()
-// })
